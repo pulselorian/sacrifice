77bc139f0077ba8c0fdfaa0fc88db6beab9ad325
pulselorian <pulselorian@gmail.com>
2022-02-26 17:47:37 -0600

    added expandable collapsable grid to include additional transaction info, minor css changes

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const numeral = require('numeral');
-var forceSsl = require('force-ssl-heroku');
+const forceSsl = require('force-ssl-heroku');
+const _ = require('lodash');
 
 app.locals.numeral = numeral;
+app.locals._ = _;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
