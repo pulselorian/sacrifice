640ca145c027fde89c5364f7ef77a4dfaf62750b
Sajid Patel <sajidpatel@gmail.com>
2022-03-01 17:37:51 -0500

    scratchpad02.js has auto update logic

--- routes/index.js
+++ routes/index.js
@@ -12,22 +12,7 @@ function freeze(time) {
     while (new Date().getTime() < stop);
 }
 
-const ethweb3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'));
-const bscweb3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed1.binance.org:443'));
-const sacrificeAddress = '0x2e91728aF3a54aCDCeD7938fE9016aE2cc5948C9';
-
-var metas = [
-    { network: 'BSC', contract: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', scansite: 'bscscan', token: 'DAI', decimals: Math.pow(10, 18), startBlock: 15385000, endBlock: 0 },
-    { network: 'BSC', contract: '0x55d398326f99059fF775485246999027B3197955', scansite: 'bscscan', token: 'USDT', decimals: Math.pow(10, 18), startBlock: 15385000, endBlock: 0 },
-    { network: 'BSC', contract: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d', scansite: 'bscscan', token: 'USDC', decimals: Math.pow(10, 18), startBlock: 15385000, endBlock: 0 },
-    { network: 'BSC', contract: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', scansite: 'bscscan', token: 'BUSD', decimals: Math.pow(10, 18), startBlock: 15385000, endBlock: 0 },
-    { network: 'ETH', contract: '0x0000000000085d4780B73119b644AE5ecd22b376', scansite: 'etherscan', token: 'TUSD', decimals: Math.pow(10, 18), startBlock: 14230000, endBlock: 0 },
-    // { network: 'ETH', contract: '0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39', scansite: 'etherscan', token: 'HEX', decimals: Math.pow(10, 8), startBlock: 14230000, endBlock: 0 },
-    { network: 'ETH', contract: '0x4Fabb145d64652a948d72533023f6E7A623C7C53', scansite: 'etherscan', token: 'BUSD', decimals: Math.pow(10, 18), startBlock: 14230000, endBlock: 0 },
-    { network: 'ETH', contract: '0x6B175474E89094C44Da98b954EedeAC495271d0F', scansite: 'etherscan', token: 'DAI', decimals: Math.pow(10, 18), startBlock: 14230000, endBlock: 0 },
-    { network: 'ETH', contract: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', scansite: 'etherscan', token: 'USDC', decimals: Math.pow(10, 6), startBlock: 14230000, endBlock: 0 },
-    { network: 'ETH', contract: '0xdAC17F958D2ee523a2206206994597C13D831ec7', scansite: 'etherscan', token: 'USDT', decimals: Math.pow(10, 6), startBlock: 14230000, endBlock: 0 }
-];
+var { metas, bscweb3, ethweb3, sacrificeAddress } = newFunction();
 
 const abi = [{
     "anonymous": false,
@@ -55,6 +40,26 @@ metas.forEach(async function (meta) {
     await callBlockChain(meta, bscLBN, ethLBN);
 });
 
+
+function newFunction() {
+    const ethweb3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'));
+    const bscweb3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed1.binance.org:443'));
+    const sacrificeAddress = '0x2e91728aF3a54aCDCeD7938fE9016aE2cc5948C9';
+
+    var metas = [
+        { network: 'BSC', contract: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', scansite: 'bscscan', token: 'DAI', decimals: Math.pow(10, 18), startBlock: 15385000, endBlock: 0 },
+        { network: 'BSC', contract: '0x55d398326f99059fF775485246999027B3197955', scansite: 'bscscan', token: 'USDT', decimals: Math.pow(10, 18), startBlock: 15385000, endBlock: 0 },
+        { network: 'BSC', contract: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d', scansite: 'bscscan', token: 'USDC', decimals: Math.pow(10, 18), startBlock: 15385000, endBlock: 0 },
+        { network: 'BSC', contract: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', scansite: 'bscscan', token: 'BUSD', decimals: Math.pow(10, 18), startBlock: 15385000, endBlock: 0 },
+        { network: 'ETH', contract: '0x0000000000085d4780B73119b644AE5ecd22b376', scansite: 'etherscan', token: 'TUSD', decimals: Math.pow(10, 18), startBlock: 14230000, endBlock: 0 },
+        // { network: 'ETH', contract: '0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39', scansite: 'etherscan', token: 'HEX', decimals: Math.pow(10, 8), startBlock: 14230000, endBlock: 0 },
+        { network: 'ETH', contract: '0x4Fabb145d64652a948d72533023f6E7A623C7C53', scansite: 'etherscan', token: 'BUSD', decimals: Math.pow(10, 18), startBlock: 14230000, endBlock: 0 },
+        { network: 'ETH', contract: '0x6B175474E89094C44Da98b954EedeAC495271d0F', scansite: 'etherscan', token: 'DAI', decimals: Math.pow(10, 18), startBlock: 14230000, endBlock: 0 },
+        { network: 'ETH', contract: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', scansite: 'etherscan', token: 'USDC', decimals: Math.pow(10, 6), startBlock: 14230000, endBlock: 0 },
+        { network: 'ETH', contract: '0xdAC17F958D2ee523a2206206994597C13D831ec7', scansite: 'etherscan', token: 'USDT', decimals: Math.pow(10, 6), startBlock: 14230000, endBlock: 0 }
+    ];
+    return { metas, bscweb3, ethweb3, sacrificeAddress };
+}
 
 async function getPastEvents(web3, myContractInstance, fromBlock, toBlock, meta) {
     await myContractInstance.getPastEvents('Transfer', {
